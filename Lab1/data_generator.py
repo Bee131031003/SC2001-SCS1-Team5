130a49994374e81689ccf02bab0e4097bd56599a
Lin Ziqi <linziqi1101@gmail.com>
2026-09-17 21:42:53 +0800

    Refactor generate_random_array to use fixed max_val

--- Lab1/data_generator.py
+++ Lab1/data_generator.py
@@ -1,14 +1,9 @@
 import random
 
-def generate_random_array(size, max_val=None):
-    # if no input value for max_val, max_val = size
-    if max_val is None:
-        max_val = size
-
+def generate_random_array(size):
+    max_val = 10000000
     result_array = []
-
-    for i in range(size):
+    for step in range(size):
         random_num = random.randint(1, max_val)
         result_array.append(random_num)
-
     return result_array
